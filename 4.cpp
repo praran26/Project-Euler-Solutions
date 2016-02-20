3b5c8e64b69591482d9b08e58fd5a261c13bbdf1
praran26 <pranjal1234.pj@gmail.com>
2016-02-20 06:42:25 +0530

    Added solution to problem 4

---
+++ 4.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vvi vector<vi>
+#define Max(a,b) ((a)>(b)?(a):(b))
+#define Min(a,b) ((a)<(b)?(a):(b))
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define all(a) a.begin(),a.end()
+#define sum(a) accumulate(all(a),0)
+#define endl '\n'
+#define hell 1000000007
+#define DEBUG true
+using namespace std;
+template <class X>
+void input(vector<X>&a,int N){
+    X temp;
+    rep(i,0,N){
+        cin>>temp;
+        a.push_back(temp);
+    }
+}
+bool isPalindrome(int x) {
+  if (x < 0) return false;
+  int div = 1;
+  while (x / div >= 10) {
+    div *= 10;
+  }
+  while (x != 0) {
+    int l = x / div;
+    int r = x % 10;
+    if (l != r) return false;
+    x = (x % div) / 10;
+    div /= 100;
+  }
+  return true;
+}
+void solve(){
+    vector<int>palindromeMultiples;
+    for(int i=999;i>99;i--){
+        for(int j=999;j>99;j--){
+            int k=i*j;
+            if(isPalindrome(k))palindromeMultiples.pb(k);
+        }
+    }
+    cout<<*max_element(all(palindromeMultiples));
+}
+
+int main(){
+	int t=1;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
