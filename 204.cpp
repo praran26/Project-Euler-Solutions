9d829f07631d72cf4b5d23b36083b453ede65b0f
Pranjal Jain <pranjal.jain.cse15@iitbhu.ac.in>
2018-05-04 11:56:28 +0530

    Optimized 204

--- 204.cpp
+++ 204.cpp
@@ -3,14 +3,7 @@
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #include "bits/stdc++.h"
 
-#ifdef PRINTERS
-#include "printers.hpp"
-using namespace printers;
-#define tr(a)		cerr<<#a<<" : "<<a<<endl
-#else
-#define tr(a)    
-#endif
-
+#define int			long long
 #define ll          long long
 #define pb          push_back
 #define mp          make_pair
@@ -23,28 +16,39 @@ using namespace printers;
 #define hell        1000000007
 #define endl        '\n'
 #define rep(i,a,b)	for(int i=a;i<b;i++)
+#define N			1000000000LL
+#define K			100LL
 using namespace std;
 
-vi primes={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-int cnt=sz(primes);
-vector<int>s;
-
-void dfs(int idx,ll num){
-	if(idx==cnt){
-		s.emplace_back(num);
+vi primes={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}; // primes <= K
+int totcnt=0;
+vi s;
+ll ans=0;
+void dfs(int idx,ll num,bool type){
+	if(idx>=totcnt){
+		if(!type)s.emplace_back(num);
+		else{
+			ans+=upper_bound(all(s),N/num)-s.begin();
+		}
 		return;
 	}
-	for(;num<=hell-7;num*=primes[idx]){
-		dfs(idx+1,num);
+	for(;num<=N;num*=primes[idx]){
+		dfs(idx+2,num,type);
+		if(num>N/primes[idx])break;
 	}
 }
 
 void solve(){
-	dfs(0,1);
-	cout<<sz(s);
+	rep(i,0,sz(primes)){
+		if(K>=primes[i])totcnt++;
+	}
+	dfs(0,1,0);
+	sort(all(s));
+	dfs(1,1,1);
+	cout<<ans;
 }
 
-int main(){
+signed main(){
 	// ios_base::sync_with_stdio(false);
 	// cin.tie(0);
 	// cout.tie(0);
